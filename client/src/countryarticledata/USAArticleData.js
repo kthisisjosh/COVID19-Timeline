cbf0a40776d28374517804048612fbcf0689c827
Joshua <jbautista396@gmail.com>
2020-03-28 23:08:31 -0400

    Add USA article data and update footer

--- client/src/countryarticledata/USAArticleData.js
+++ client/src/countryarticledata/USAArticleData.js
@@ -69,7 +69,12 @@ const data = [
         {
             day: 11,
             articles: [
-
+                {
+                    content: "Coronavirus gets official name from WHO: COVID-19.",
+                    img: "articleImages/virus.jpeg",
+                    link: "https://www.nbcnews.com/health/health-news/coronavirus-gets-official-name-who-covid-19-n1134756",
+                    credit: null
+                }
             ]
         },
         {
@@ -152,7 +157,12 @@ const data = [
         {
             day: 24,
             articles: [
-
+                {
+                    content: "Dow closes down 1,000 points as coronavirus fears slam Wall Street",
+                    img: "",
+                    link: "https://www.nbcnews.com/business/markets/dow-plunges-950-points-fears-coronavirus-will-tank-global-economic-n1141546",
+                    credit: ""
+                }
             ]
         },
         {
@@ -164,7 +174,12 @@ const data = [
         {
             day: 26,
             articles: [
-
+                {
+                    content: "California announced the first case in the U.S. with no clear source of exposure.",
+                    img: "",
+                    link: "https://www.nbcnews.com/health/health-news/after-mystery-coronavirus-case-california-health-officials-go-detective-mode-n1144231",
+                    credit: ""
+                }
             ]
         },
         {
@@ -182,7 +197,18 @@ const data = [
         {
             day: 29,
             articles: [
-                
+                {
+                    content: "President Donald Trump announced additional travel restrictions involving Iran and increased warnings about travel to Italy and South Korea.",
+                    img: "",
+                    link: "https://www.nbcnews.com/politics/donald-trump/coronavirus-trump-give-update-white-house-saturday-n1145921",
+                    credit: ""
+                },
+                {
+                    content: "The first recorded coronavirus death in the U.S., a man in his 50s in Washington state.",
+                    img: "",
+                    link: "https://www.nbcnews.com/news/us-news/1st-coronavirus-death-u-s-officials-say-n1145931",
+                    credit: ""
+                }
             ]
         },
     ],
@@ -205,7 +231,12 @@ const data = [
         {
             day: 3,
             articles: [
-                
+                {
+                    content: "Patients infected by the coronavirus wait to be transferred from Wuhan No. 5 Hospital to Leishenshan Hospital, the newly-built hospital for COVID-19 patients, in Wuhan.",
+                    img: "",
+                    link: "https://www.nbcnews.com/politics/congress/senate-passes-8-3-billion-emergency-bill-combat-coronavirus-n1150521",
+                    credit: ""
+                }
             ]
         },
         {
@@ -223,7 +254,18 @@ const data = [
         {
             day: 6,
             articles: [
-                
+                {
+                    content: "Trump signed an $8.3 billion emergency spending package to combat the coronavirus outbreak, as the number of global cases hit 100,000.",
+                    img: "",
+                    link: "https://www.nbcnews.com/politics/congress/senate-passes-8-3-billion-emergency-bill-combat-coronavirus-n1150521",
+                    credit: ""
+                },
+                {
+                    content: "Vice President Mike Pence announced that 21 people aboard the Grand Princess, a cruise ship being held off the coast of California, tested positive for the coronavirus.",
+                    img: "",
+                    link: "https://www.nbcnews.com/politics/politics-news/21-people-grand-princess-cruise-ship-test-positive-coronavirus-pence-n1151936",
+                    credit: ""
+                }
             ]
         },
         {
@@ -235,7 +277,12 @@ const data = [
         {
             day: 8,
             articles: [
-
+                {
+                    content: "Confirmed cases in the U.S. topped 500.",
+                    img: "",
+                    link: "https://coronavirus.jhu.edu/map.html",
+                    credit: ""
+                }
             ]
         },
         {
@@ -253,19 +300,82 @@ const data = [
         {
             day: 11,
             articles: [
-                
+                {
+                    content: "The World Health Organization declared that the coronavirus outbreak “can be characterized as a pandemic,” which is defined as worldwide spread of a new disease for which most people do not have immunity.",
+                    img: "",
+                    link: "https://www.nbcnews.com/health/health-news/live-blog/coronavirus-updates-live-u-s-cases-top-1-000-spread-n1155241/ncrd1155646#liveBlogHeader",
+                    credit: ""
+                },
+                {
+                    content: "The NBA suspended all basketball games after a player for the Utah Jazz preliminarily tested positive for COVID-19, the disease caused by the new coronavirus.",
+                    img: "",
+                    link: "https://www.nbcnews.com/news/us-news/nba-suspends-remainder-season-coronavirus-spreads-n1156156",
+                    credit: ""
+                },
+                {
+                    content: "The Oscar-winning actor Tom Hanks and his wife, Rita Wilson, announced that they tested positive for the coronavirus. They announced on Instagram that they are being isolated and observed in Australia, where Hanks was in pre-production for a film.",
+                    img: "",
+                    link: "https://www.instagram.com/p/B9qBEyjJu4B/?utm_source=ig_embed",
+                    credit: ""
+                },
+                {
+                    content: "Trump announced a new restriction on many foreign travelers from 26 countries in Europe, except for Ireland and the United Kingdom, for the next 30 days.",
+                    img: "",
+                    link: "https://www.nbcnews.com/politics/donald-trump/trump-make-prime-time-address-coronavirus-wednesday-night-n1155941",
+                    credit: ""
+                },
+                {
+                    content: "",
+                    img: "",
+                    link: "",
+                    credit: ""
+                },
             ]
         },
         {
             day: 12,
             articles: [
-                
+                {
+                    content: "MLB announced that it will suspend spring training and delay the start of the regular baseball season by at least two weeks.",
+                    img: "",
+                    link: "https://www.nbcnews.com/health/health-news/live-blog/coronavirus-updates-live-u-s-slaps-travel-restrictions-most-europe-n1156266/ncrd1157221#liveBlogHeader",
+                    credit: ""
+                },
+                {
+                    content: "The NHL announced that it will pause its hockey season. The league’s commissioner did not set an end date for the suspension.",
+                    img: "",
+                    link: "",
+                    credit: ""
+                },
+                {
+                    content: "The NCAA canceled both the men’s and women’s college basketball tournaments, known as March Madness, after most conferences suspended their postseason tournaments.",
+                    img: "",
+                    link: "https://www.nbcnews.com/news/us-news/ncaa-cancels-march-madness-tournaments-n1157261",
+                    credit: ""
+                }
             ]
         },
         {
             day: 13,
             articles: [
-                
+                {
+                    content: "Trump declared a national state of emergency that could free up $50 billion to help fight the pandemic.",
+                    img: "",
+                    link: "https://www.nbcnews.com/politics/donald-trump/trump-hold-friday-afternoon-press-conference-coronavirus-n1157981",
+                    credit: ""
+                },
+                {
+                    content: "Trump tweeted that some cruise lines, including Princess Cruises, Norwegian and Royal Caribbean, will suspend outbound trips, at his request, for 30 days.",
+                    img: "",
+                    link: "https://twitter.com/realDonaldTrump/status/1238590988593086464?ref_src=twsrc%5Etfw",
+                    credit: ""
+                },
+                {
+                    content: "States across the U.S., including Michigan, Pennsylvania and Maryland, announced plans to close schools over the coronavirus concerns.",
+                    img: "",
+                    link: "",
+                    credit: ""
+                }
             ]
         },
         {
@@ -277,49 +387,227 @@ const data = [
         {
             day: 15,
             articles: [
-                
+                {
+                    content: "The White House announced that the European travel ban would be extended to include the U.K. and Ireland.",
+                    img: "",
+                    link: "https://www.nbcnews.com/health/health-news/live-blog/coronavirus-updates-live-house-approves-coronavirus-aid-bill-n1158821/ncrd1159006#liveBlogHeader",
+                    credit: ""
+                },
+                {
+                    content: "The number of confirmed cases in the U.S. surpassed 3,000, with New York, California and Washington recording the most confirmed cases. The national death toll rose to 61.",
+                    img: "",
+                    link: "",
+                    credit: ""
+                },
+                {
+                    content: "Twenty-nine additional states, including New York, Massachusetts, South Carolina and Hawaii, announced school closures.",
+                    img: "",
+                    link: "",
+                    credit: ""
+                },
+                {
+                    content: "The Centers for Disease Control and Prevention released guidelines recommending \"that for the next 8 weeks, organizers (whether groups or individuals) cancel or postpone in-person events that consist of 50 people or more throughout the United States.\"",
+                    img: "",
+                    link: "",
+                    credit: ""
+                },
             ]
         },
         {
             day: 16,
             articles: [
-                
+                {
+                    content: "Wall Street plunged again, as the Dow Jones Industrial Average sank by 3,000 points and the S&P 500 and Nasdaq were down by around 12 percent by the closing bell.",
+                    img: "",
+                    link: "",
+                    credit: ""
+                },
+                {
+                    content: "San Francisco imposed strict prohibitions on residents leaving their homes “except for essential needs,” becoming the first city in the U.S. to introduce such extreme measures in response to the pandemic.",
+                    img: "",
+                    link: "https://www.nbcnews.com/health/health-news/san-francisco-require-people-stay-home-except-essential-needs-n1160916",
+                    credit: ""
+                },
+                {
+                    content: "MLB announced that the start of the season will be pushed back eight weeks, per guidance from the CDC.",
+                    img: "",
+                    link: "",
+                    credit: ""
+                },
+                {
+                    content: "President Trump advised all Americans to avoid gatherings of 10 or more people, to avoid going to bars and restaurants and to halt discretionary travel. The guidelines, from the administration’s coronavirus task force, will remain in effect for 15 days.",
+                    img: "",
+                    link: "",
+                    credit: ""
+                },
+                {
+                    content: "U.S. researchers administered the first shot to the first person in a test of an experimental coronavirus vaccine. Even if the trials go well, health officials warned that a vaccine would not be widely available for at least 12 to 18 months.",
+                    img: "",
+                    link: "",
+                    credit: ""
+                },
+                {
+                    content: "NASCAR announced it would postpone all races until at least the beginning of May.",
+                    img: "",
+                    link: "",
+                    credit: ""
+                }
             ]
         },
         {
             day: 17,
             articles: [
-                
+                {
+                    content: "West Virginia, the last state in the U.S. without a confirmed coronavirus case, recorded its first. Confirmed cases across the country rose to more than 5,800 and the death toll surpassed 100.",
+                    img: "",
+                    link: "",
+                    credit: ""
+                },
+                {
+                    content: "Maryland’s governor postponed the state’s primary election, a day after Ohio’s primary was called also off. Florida and Illinois and Arizona proceeded with their primaries.",
+                    img: "",
+                    link: "https://www.nbcnews.com/health/health-news/live-blog/coronavirus-updates-countries-across-world-step-restrictions-combat-crisis-n1161301/ncrd1161661#liveBlogHeader",
+                    credit: ""
+                }
             ]
         },
         {
             day: 18,
             articles: [
-               
+               {
+                    content: "Canada and the U.S. agreed to close its borders to all “non-essential traffic.”",
+                    img: "",
+                    link: "https://www.nbcnews.com/politics/donald-trump/u-s-canada-agree-close-border-non-essential-traffic-trump-n1162796",
+                    credit: ""
+                },
+                {
+                    content: "The Trump administration suspended refugee admissions untill April 6 due to the coronavirus pandemic.",
+                    img: "",
+                    link: "",
+                    credit: ""
+                }
             ]
         },
         {
             day: 19,
             articles: [
-               
+               {
+                   content: "President Trump signed a coronavirus aid bill into law. The Families First Coronavirus Response Act would provide free coronavirus testing and ensure paid emergency leave for those infected or caring for a family member with the illness, while also providing additional Medicaid funding, food assistance and unemployment benefits.",
+                   img: "",
+                   link: "https://www.nbcnews.com/politics/congress/senate-plans-vote-house-coronavirus-bill-wednesday-n1162851",
+                   credit: ""
+               },
+               {
+                    content: "The U.S. State Department raised the global travel advisory to Level 4: Do Not Travel, warning Americans against traveling internationally and for those abroad to consider returning immediately.",
+                    img: "",
+                    link: "https://www.nbcnews.com/news/us-news/coronavirus-state-department-raise-travel-warnings-americans-n1164021",
+                    credit: ""
+               },
+               {
+                   content: "California issued a statewide stay-at-home order asking residents to only leave the house if necessary.",
+                   img: "",
+                   link: "",
+                   credit: ""
+               },
+               {
+                   content: "Connecticut delayed its presidential primary election to June.",
+                   img: "",
+                   link: "",
+                   credit: ""
+               }
             ]
         },
         {
             day: 20,
             articles: [
-                
+                {
+                   content: "The U.S. announced plans to close the border with Mexico to all “nonessential travel.” Acting Homeland Security Secretary Chad Wolf said all immigrants who lack proper entry documentation will be turned away.",
+                   img: "",
+                   link: "",
+                   credit: ""
+               },
+               {
+                   content: "Cases in California exceeded 1,000, more than doubling from only three days ago. The new figures came one day after Gov. Gavin Newsom issued a statewide “stay-at-home” order for the state’s 40 million residents.",
+                   img: "",
+                   link: "",
+                   credit: ""
+               },
+               {
+                   content: "The Dow Jones Industrial Average sank by 916 points and the S&P 500 closed the day down 4.3 percent, marking its worst weekly performance since the 2008 financial crisis.",
+                   img: "",
+                   link: "",
+                   credit: ""
+               }
             ]
         },
         {
             day: 21,
             articles: [
-                
+                {
+                   content: "Coronavirus cases in New York State, the hardest-hit in the U.S., surpassed 10,000.",
+                   img: "",
+                   link: "https://www.nbcnews.com/news/us-news/coronavirus-cases-new-york-state-now-top-10-000-n1165626",
+                   credit: ""
+               },
+               {
+                   content: "Gov. Phil Murphy of New Jersey issued a stay-at-home order for nearly all of the state's 9 million residents..",
+                   img: "",
+                   link: "https://www.nbcnews.com/news/us-news/new-jersey-gov-phil-murphy-issues-stay-home-order-nearly-n1165661",
+                   credit: ""
+               },
+               {
+                   content: "Sen. Rand Paul, R-Ky., became the first known senator to test positive for coronavirus.",
+                   img: "",
+                   link: "",
+                   credit: ""
+               },
+               {
+                   content: "Ohio Gov. Mike DeWine issued a new stay-at-home order for all non-essential workers.",
+                   img: "",
+                   link: "",
+                   credit: ""
+               }
             ]
         },
         {
             day: 22,
             articles: [
-                
+                {
+                   content: "",
+                   img: "",
+                   link: "",
+                   credit: ""
+               },
+               {
+                   content: "",
+                   img: "",
+                   link: "",
+                   credit: ""
+               },
+               {
+                   content: "",
+                   img: "",
+                   link: "",
+                   credit: ""
+               },
+               {
+                   content: "",
+                   img: "",
+                   link: "",
+                   credit: ""
+               },
+               {
+                   content: "",
+                   img: "",
+                   link: "",
+                   credit: ""
+               },
+               {
+                   content: "",
+                   img: "",
+                   link: "",
+                   credit: ""
+               }
             ]
         },
         {
