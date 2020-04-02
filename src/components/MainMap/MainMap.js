7afc500bdfe0064a14790b7353e001c80ce674f9
Joshua <jbautista396@gmail.com>
2020-04-01 20:15:14 -0400

    Update mapbox token

--- src/components/MainMap/MainMap.js
+++ src/components/MainMap/MainMap.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Map } from 'react-leaflet';
 import MapBoxGLLayer from "./MapBoxGLLayer";
 
-const MAPBOX_ACCESS_TOKEN = "pk.eyJ1Ijoia3RoaXNpc2pvc2giLCJhIjoiY2s4ZXNkZDg4MDAwdzNkcXNzdmF2eWU4ZiJ9.qzJgKqXH2iL4zDtUdywlDw";
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1Ijoia3RoaXNpc2pvc2giLCJhIjoiY2s4aTBjMnN6MDExdDNncGRxdDhhZm1ieCJ9.XJL7BNx6ZC6N4ugrh9mPmg";
 
 class MainMap extends Component {
     constructor(props) {
